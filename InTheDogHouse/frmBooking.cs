176064001d484334e9f0c7775905251eb8c5457d
Colm Canavan <30004001@live.NWRC.ac.uk>
2021-10-11 14:12:07 +0100

    Revert "xdfsdf"
    This reverts commit ab742de3709e24bb3580ce7f55a8127220cc5024.

--- InTheDogHouse/frmBooking.cs
+++ InTheDogHouse/frmBooking.cs
@@ -14,15 +14,13 @@ namespace InTheDogHouse
 {
     public partial class frmBooking : Form
     {
-        SqlDataAdapter daCustomer, daDog, daKennels, daBookedKennels, daBooking, daBookingDet;
+        SqlDataAdapter daCustomer, daDog, daKennel;
         DataSet dsInTheDogHouse = new DataSet();
-        SqlCommandBuilder cmdBBookedKennels, cmdBBooking, cmdBBookingDet;
-        SqlCommand cmdKennelDetails;
-        string sqlCustomer, sqlDog, sqlKennel, sqlKennelDetails, sqlBookedKennels, sqlBooking, sqlBookingDet;
+        SqlCommandBuilder sqlBCustomer;
+        string sqlCustomer, sqlDog, sqlKennel;
         int selectedTab = 0;
         bool custSelected = false;
         int custNoSelected = 0;
-        SqlConnection conn;
         frmContainer container;
 
         public frmBooking(frmContainer container)
@@ -33,61 +31,38 @@ namespace InTheDogHouse
 
         private void frmBooking_Load(object sender, EventArgs e)
         {
-            lblBookingRange.Text = DateTime.Now.ToShortDateString();
-            dtpStart.MinDate = DateTime.Now;
-            conn = new SqlConnection(container.connStr);
+
             sqlCustomer = @"select * from Customer";
-            daCustomer = new SqlDataAdapter(sqlCustomer, conn);
+            daCustomer = new SqlDataAdapter(sqlCustomer, container.connStr);
             daCustomer.FillSchema(dsInTheDogHouse, SchemaType.Source, "Customer");
             daCustomer.Fill(dsInTheDogHouse, "Customer");
 
 
             sqlDog = @"select * from Dog";
-            daDog = new SqlDataAdapter(sqlDog, conn);
+            daDog = new SqlDataAdapter(sqlDog, container.connStr);
             daDog.FillSchema(dsInTheDogHouse, SchemaType.Source, "Dog");
             daDog.Fill(dsInTheDogHouse, "Dog");
 
+            sqlKennel = @"select * from Kennel";
+            daKennel = new SqlDataAdapter(sqlKennel, container.connStr);
+            daKennel.FillSchema(dsInTheDogHouse, SchemaType.Source, "Kennel");
+            daKennel.Fill(dsInTheDogHouse, "Kennel");
 
 
             dsInTheDogHouse.Tables["Customer"].Columns.Add("ComboDisplay", typeof(string), "CustomerNo + ' - ' + Forename + ' ' + Surname");
+            dsInTheDogHouse.Tables["Kennel"].Columns.Add("ComboDisplay", typeof(string), "KennelNo + ' - ' + SizeK");
             dsInTheDogHouse.Tables["Dog"].Columns.Add("ComboDisplay", typeof(string), "DogNo + ' - ' + Name");
 
             lstCustomer.DisplayMember = "ComboDisplay";
             lstCustomer.ValueMember = "CustomerNo";
             lstCustomer.DataSource = dsInTheDogHouse.Tables["Customer"].DefaultView;
 
-            sqlKennelDetails = @"SELECT distinct kennelNo, sizeK, sizeNo, sizeB, dog.breedNo, chargePerDay FROM kennel Join size on sizeK = sizeNo join breed on sizeNo = sizeB join dog on dog.breedNo = breed.breedNo where dog.breedNo = @BreedNo order by kennelNo";
-            cmdKennelDetails = new SqlCommand(sqlKennelDetails, conn);
-            cmdKennelDetails.Parameters.Add("@BreedNo", SqlDbType.Int);
-            daKennels = new SqlDataAdapter(cmdKennelDetails);
-            daKennels.FillSchema(dsInTheDogHouse, SchemaType.Source, "Kennel");
-
-            sqlBookedKennels = @"SELECT booking.bookingNo, dateStart, noDays, dogNo, kennelNo FROM booking JOIN bookingDetail on booking.bookingNo = bookingDetail.bookingNO order by bookingNo";
-            daBookedKennels = new SqlDataAdapter(sqlBookedKennels, conn);
-            cmdBBookedKennels = new SqlCommandBuilder(daBookedKennels);
-            daBookedKennels.FillSchema(dsInTheDogHouse, SchemaType.Source, "BookedKennels");
-            daBookedKennels.Fill(dsInTheDogHouse, "BookedKennels");
-
-            sqlBooking = @"SELECT * from Booking";
-            daBooking = new SqlDataAdapter(sqlBooking, conn);
-            cmdBBooking = new SqlCommandBuilder(daBooking);
-            daBooking.FillSchema(dsInTheDogHouse, SchemaType.Source, "Booking");
-            daBooking.Fill(dsInTheDogHouse, "Booking");
-
-            sqlBookingDet = @"SELECT * from bookingDetail";
-            daBookingDet = new SqlDataAdapter(sqlBookingDet, conn);
-            cmdBBookingDet = new SqlCommandBuilder(daBookingDet);
-            daBookingDet.FillSchema(dsInTheDogHouse, SchemaType.Source, "bookingDetail");
-            daBookingDet.Fill(dsInTheDogHouse, "bookingDetail");
         }
         private void txtCustomer_TextChanged(object sender, EventArgs e)
         {
 
             dsInTheDogHouse.Tables["Customer"].DefaultView.RowFilter = "ComboDisplay LIKE '%" + txtCustomer.Text + "%'";
-            if (lstCustomer.Items.Count > 0)
-            {
-                lstCustomer.SelectedIndex = 0;                
-            }
+            lstCustomer.SelectedIndex = 0;
             updateDogList();
         }
 
